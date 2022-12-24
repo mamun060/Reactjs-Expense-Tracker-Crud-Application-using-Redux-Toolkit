d96698f36801d159609f3fc43528baedb898e9e9
mamun060 <obujraju420@gmail.com>
2022-12-24 12:13:53 +0600

    add transection slice & all logic

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import transectionReducer from "../features/transaction/transectionSlice";
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        transection: transectionReducer,
+    },
 });
---
+++ src/features/transaction/transectionSlice.js
@@ -0,0 +1,109 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { addTransaction, deleteTransaction, editTransaction, getTransactions } from "./transactionAPI";
+
+// our state create
+
+const initialState = {
+    transections: [],
+    isLoading: false,
+    isError: false,
+    error: ""
+}
+
+// create fetch request 
+export const fetchTransectios = createAsyncThunk('transection/fetchTransections', async ()=> {
+    const transections = await getTransactions();
+    return transections;
+});
+
+export const addTransections = createAsyncThunk('transection/addTransections', async(data)=>{
+    const transections = await addTransaction();
+    return transections;
+})
+
+export const editTransections = createAsyncThunk('transection/editTransections', async({id, data})=>{
+    const transections = await editTransaction(id, data);
+    return transections;
+})
+
+export const deleteTransections = createAsyncThunk('transection/deleteTransections', async (id)=> {
+    const transections = await deleteTransaction(id);
+    return transections;
+})
+
+// create slice 
+
+const transectionSlice = createSlice({
+    name: "transection",
+    initialState,
+    extraReducers: (builder)=> {
+        builder
+        .addCase( fetchTransectios.pending, (state)=>{
+            state.isError = false;
+            state.isLoading = true;
+        })
+        .addCase(fetchTransectios.fulfilled, (state, action)=>{
+            state.isError = false;
+            state.isLoading = false;
+            state.transections = action.payload;
+        })
+        .addCase(fetchTransectios.rejected, (state, action)=>{
+            state.isLoading = false;
+            state.isError = true;
+            state.error = action.error?.message;
+            state.transections = [];
+        })
+        .addCase( addTransections.pending, (state)=>{
+            state.isError = false;
+            state.isLoading = true;
+        })
+        .addCase( addTransections.fulfilled, (state, action)=>{
+            state.isError = false;
+            state.isLoading = false;
+            state.transections.push(action.payload);
+        })
+        .addCase( addTransections.rejected, (state, action)=>{
+            state.isLoading = false;
+            state.isError = true;
+            state.error = action.error?.message;
+        })
+        .addCase( editTransections.pending, (state)=>{
+            state.isError = false;
+            state.isLoading = true;
+        })
+        .addCase( editTransections.fulfilled, (state, action)=>{
+            state.isError = false;
+            state.isLoading = false;
+            const indexToUpdate = state.transections.findIndex(
+                (t)=> t.id === action.payload.id
+            )
+            state.transections[indexToUpdate] = action.payload;
+        })
+        .addCase( editTransections.rejected, (state, action)=>{
+            state.isLoading = false;
+            state.isError = true;
+            state.error = action.error?.message;
+        })
+        .addCase( deleteTransections.pending, (state)=>{
+            state.isError = false;
+            state.isLoading = true;
+        })
+        .addCase( deleteTransections.fulfilled, (state, action)=>{
+            state.isError = false;
+            state.isLoading = false;
+
+            state.transections = state.transections.filter(
+                (t)=> t.id !== action.payload
+            );
+        })
+        .addCase( deleteTransections.rejected, (state, action)=>{
+            state.isLoading = false;
+            state.isError = true;
+            state.error = action.error?.message;
+        })
+    }
+})
+
+
+// export file
+export default transectionSlice.reducer;
