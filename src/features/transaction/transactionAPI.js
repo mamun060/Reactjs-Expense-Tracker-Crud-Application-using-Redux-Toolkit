559dde4e66a5bcb9b8d052ab4aaceac2bccfc7d6
mamun060 <obujraju420@gmail.com>
2022-12-24 10:39:02 +0600

    create api list

--- src/features/transaction/transactionAPI.js
+++ src/features/transaction/transactionAPI.js
@@ -1,25 +1,28 @@
 import axios from "../../utils/axios";
 
+// create get request
 export const getTransactions = async () => {
     const response = await axios.get("/transactions");
-
     return response.data;
 };
+
+
+// create post request api
 
 export const addTransaction = async (data) => {
     const response = await axios.post("/transactions", data);
-
     return response.data;
 };
+
+// create put request api 
 
 export const editTransaction = async (id, data) => {
     const response = await axios.put(`/transactions/${id}`, data);
-
     return response.data;
 };
 
+// create delete request api
 export const deleteTransaction = async (id) => {
     const response = axios.delete(`/transactions/${id}`);
-
     return response.data;
 };
